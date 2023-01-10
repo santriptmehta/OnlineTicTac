458e3dc854675f40aee145ddda9c11190dacea46
santript <santriptmehta@gmail.com>
2023-01-10 20:05:57 +0530

    Initial commit

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_blankspace_onlinetictactoe_TicTacToeApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.blankspace.onlinetictactoe.TicTacToeApp"
+)
+public final class _com_blankspace_onlinetictactoe_TicTacToeApp {
+}
---
+++ app/build/generated/source/kapt/debug/com/blankspace/onlinetictactoe/TicTacToeApp_GeneratedInjector.java
@@ -0,0 +1,15 @@
+package com.blankspace.onlinetictactoe;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+
+@OriginatingElement(
+    topLevelClass = TicTacToeApp.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+public interface TicTacToeApp_GeneratedInjector {
+  void injectTicTacToeApp(TicTacToeApp ticTacToeApp);
+}
