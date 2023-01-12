85e43aa6eb7004e3005e4e5a43fb05edc5830a54
Santript Mehta <46135632+santriptmehta@users.noreply.github.com>
2023-01-12 12:55:07 +0530

    initial delete

---
+++ app/build/generated/hilt/component_sources/debug/com/blankspace/onlinetictactoe/Hilt_TicTacToeApp.java
@@ -0,0 +1,52 @@
+package com.blankspace.onlinetictactoe;
+
+import android.app.Application;
+import androidx.annotation.CallSuper;
+import dagger.hilt.android.internal.managers.ApplicationComponentManager;
+import dagger.hilt.android.internal.managers.ComponentSupplier;
+import dagger.hilt.android.internal.modules.ApplicationContextModule;
+import dagger.hilt.internal.GeneratedComponentManagerHolder;
+import dagger.hilt.internal.UnsafeCasts;
+import java.lang.Object;
+import java.lang.Override;
+
+/**
+ * A generated base class to be extended by the @dagger.hilt.android.HiltAndroidApp annotated class. If using the Gradle plugin, this is swapped as the base class via bytecode transformation.
+ */
+public abstract class Hilt_TicTacToeApp extends Application implements GeneratedComponentManagerHolder {
+  private boolean injected = false;
+
+  private final ApplicationComponentManager componentManager = new ApplicationComponentManager(new ComponentSupplier() {
+    @Override
+    public Object get() {
+      return DaggerTicTacToeApp_HiltComponents_SingletonC.builder()
+          .applicationContextModule(new ApplicationContextModule(Hilt_TicTacToeApp.this)).build();
+    }
+  });
+
+  @Override
+  public final ApplicationComponentManager componentManager() {
+    return componentManager;
+  }
+
+  @Override
+  public final Object generatedComponent() {
+    return this.componentManager().generatedComponent();
+  }
+
+  @CallSuper
+  @Override
+  public void onCreate() {
+    hiltInternalInject();
+    super.onCreate();
+  }
+
+  protected void hiltInternalInject() {
+    if (!injected) {
+      injected = true;
+      // This is a known unsafe cast, but is safe in the only correct use case:
+      // TicTacToeApp extends Hilt_TicTacToeApp
+      ((TicTacToeApp_GeneratedInjector) generatedComponent()).injectTicTacToeApp(UnsafeCasts.<TicTacToeApp>unsafeCast(this));
+    }
+  }
+}
