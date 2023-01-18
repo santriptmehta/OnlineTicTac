ab7f3d344af5c4684cd95f495ec0743dbbf53f40
Santript Mehta <46135632+santriptmehta@users.noreply.github.com>
2023-01-19 00:02:49 +0530

    added the matrix line

--- app/build/tmp/kapt3/stubs/debug/com/blankspace/onlinetictactoe/data/KtorRealtimeMessagingClient.java
+++ app/build/tmp/kapt3/stubs/debug/com/blankspace/onlinetictactoe/data/KtorRealtimeMessagingClient.java
@@ -2,9 +2,9 @@ package com.blankspace.onlinetictactoe.data;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0011\u0010\u0007\u001a\u00020\bH\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\tJ\u000e\u0010\n\u001a\b\u0012\u0004\u0012\u00020\f0\u000bH\u0016J\u0019\u0010\r\u001a\u00020\b2\u0006\u0010\u000e\u001a\u00020\u000fH\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0010R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0005\u001a\u0004\u0018\u00010\u0006X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u0011"}, d2 = {"Lcom/blankspace/onlinetictactoe/data/KtorRealtimeMessagingClient;", "Lcom/blankspace/onlinetictactoe/data/RealtimeMessagingClient;", "client", "Lio/ktor/client/HttpClient;", "(Lio/ktor/client/HttpClient;)V", "session", "Lio/ktor/http/cio/websocket/WebSocketSession;", "close", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getGameStateStream", "Lkotlinx/coroutines/flow/Flow;", "Lcom/blankspace/onlinetictactoe/data/GameState;", "sendAction", "action", "Lcom/blankspace/onlinetictactoe/data/MakeTurn;", "(Lcom/blankspace/onlinetictactoe/data/MakeTurn;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u00004\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0011\u0010\u0007\u001a\u00020\bH\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\tJ\u000e\u0010\n\u001a\b\u0012\u0004\u0012\u00020\f0\u000bH\u0016J\u0019\u0010\r\u001a\u00020\b2\u0006\u0010\u000e\u001a\u00020\u000fH\u0096@\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\u0010R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0005\u001a\u0004\u0018\u00010\u0006X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u0082\u0002\u0004\n\u0002\b\u0019\u00a8\u0006\u0011"}, d2 = {"Lcom/blankspace/onlinetictactoe/data/KtorRealtimeMessagingClient;", "Lcom/blankspace/onlinetictactoe/data/RealtimeMessagingClient;", "client", "Lio/ktor/client/HttpClient;", "(Lio/ktor/client/HttpClient;)V", "session", "Lio/ktor/http/cio/websocket/WebSocketSession;", "close", "", "(Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "getGameStateStream", "Lkotlinx/coroutines/flow/Flow;", "Lmodels/GameState;", "sendAction", "action", "Lmodels/MakeTurn;", "(Lmodels/MakeTurn;Lkotlin/coroutines/Continuation;)Ljava/lang/Object;", "app_debug"})
 public final class KtorRealtimeMessagingClient implements com.blankspace.onlinetictactoe.data.RealtimeMessagingClient {
-    private final io.ktor.client.HttpClient client = null;
+    private io.ktor.client.HttpClient client;
     private io.ktor.http.cio.websocket.WebSocketSession session;
     
     public KtorRealtimeMessagingClient(@org.jetbrains.annotations.NotNull()
@@ -14,14 +14,14 @@ public final class KtorRealtimeMessagingClient implements com.blankspace.onlinet
     
     @org.jetbrains.annotations.NotNull()
     @java.lang.Override()
-    public kotlinx.coroutines.flow.Flow<com.blankspace.onlinetictactoe.data.GameState> getGameStateStream() {
+    public kotlinx.coroutines.flow.Flow<models.GameState> getGameStateStream() {
         return null;
     }
     
     @org.jetbrains.annotations.Nullable()
     @java.lang.Override()
     public java.lang.Object sendAction(@org.jetbrains.annotations.NotNull()
-    com.blankspace.onlinetictactoe.data.MakeTurn action, @org.jetbrains.annotations.NotNull()
+    models.MakeTurn action, @org.jetbrains.annotations.NotNull()
     kotlin.coroutines.Continuation<? super kotlin.Unit> continuation) {
         return null;
     }
